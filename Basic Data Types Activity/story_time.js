98e3f572d47c44743b205a33aa0056d21f2f905c
Rhea Karnani <rheakarnani@gmail.com>
2022-07-07 23:02:47 -0400

    added story_time.js

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,35 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings = "Welcome to New York City!";
+let name = "Benji";
+let title = "Tour NYC With Me!";
+let origin = "Manhattan";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 14;
+let arrivingYear = 7;
+let numStudents = 374;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let collection = ["big apple", "broadway", "M&M store", "Times Square"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let havingFun = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Hey everyone! " + greetings);
+console.log("My name is " + name + " and I'm part of a show called " + title);
+console.log("I'm from " + origin + " and I have taken " + numStudents + " students on this tour!");
+console.log("I have been in NYC for about half my life now.");
+console.log("I am currently " + age + " and I have moved to the " + collection[0] + " at " + arrivingYear + " years old.");
+console.log("Everyone I asked who has taken my tour all said that having fun =  " + havingFun + ".");
+console.log("To start this wonderful tour, let's talk about the best things about this city.");
+console.log("My personal favorite has to be " + collection[1] + ".");
+console.log("The amount of spectacular plays I have seen are simply incredible. Highly recommend!");
+console.log("In addition, the gift shops are also amazing! The " + collection[2] + " is a must.");
+console.log("Well I will leave you to explore NYC to its fullest and I will catch you later!");
+console.log("Byeee!!");
